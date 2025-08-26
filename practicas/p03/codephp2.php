5bf787e79b3a17e380968c24609ddd0697a28320
lalit <joseeduardopozoszecua@gmail.com>
2025-08-25 23:29:39 -0600

    Se agrego codephp2.php a la carpeta de p03

---
+++ practicas/p03/codephp2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<?php
+$variable1 = " PHP 5";
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <?php
+  echo "<title>Una página llena de scripts PHP</title>";
+  ?>
+</head>
+<body>
+  <h2>Ejemplo con variable PHP:</h2>
+  <?php
+  echo "<p>Bienvenido a mi sitio con $variable1</p>";
+  ?>
+</body>
+</html>
