c3573b864c84095697efb915288fa7ecafe6b7b3
lalit <joseeduardopozoszecua@gmail.com>
2025-11-12 19:49:22 -0600

    Se creo el archivo product-single.php en la carpeta backend/

---
+++ practicas/p12/backend/product-single.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use TECWEB\MYAPI\Read;
+
+$productos = new Read('marketzone');
+$productos->single($_POST['id']);
+echo $productos->getData();
+?>
