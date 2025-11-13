b1aec9a72bd7222a7dffa14cc9f1c3c5e9fc9807
lalit <joseeduardopozoszecua@gmail.com>
2025-11-12 19:39:56 -0600

    Se creo el archivo product-search.php en la carpeta backend/

---
+++ practicas/p12/backend/product-search.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use TECWEB\MYAPI\Read;
+
+$productos = new Read('marketzone');
+$productos->search($_GET['search']);
+echo $productos->getData();
+?>
