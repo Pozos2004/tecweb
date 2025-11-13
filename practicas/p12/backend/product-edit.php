57266a4aa43aac4b679c9fc971b1293e9e44c51e
lalit <joseeduardopozoszecua@gmail.com>
2025-11-12 19:51:12 -0600

    Se creo el archivo product-edit.php en la carpeta backend/

---
+++ practicas/p12/backend/product-edit.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use TECWEB\MYAPI\Update;
+
+$productos = new Update('marketzone');
+$productos->edit(json_decode(json_encode($_POST)));
+echo $productos->getData();
+?>
