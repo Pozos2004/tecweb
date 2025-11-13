2639a64489fc2987cbaee1214f261c5e7b83ee85
lalit <joseeduardopozoszecua@gmail.com>
2025-11-12 21:09:10 -0600

    Archivos que se ocupan para la practica

---
+++ practicas/p12/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tecweb/myapi',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '22913b4e512486a802b43c793af518e8a0e72bd0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tecweb/myapi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '22913b4e512486a802b43c793af518e8a0e72bd0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
