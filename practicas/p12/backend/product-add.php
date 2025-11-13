c3982c2863066cee166486bdfe10d6cfaefb81ea
lalit <joseeduardopozoszecua@gmail.com>
2025-11-12 19:36:01 -0600

    Se creo el archivo product-add.php en la carpeta backend/

---
+++ practicas/p12/backend/product-add.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use TECWEB\MYAPI\Create;
+
+$productos = new Create('marketzone');
+$productos->add(json_decode(json_encode($_POST)));
+echo $productos->getData();
+?>
