7f197c4ed42c04b48b651b4a15b57090343223cb
lalit <joseeduardopozoszecua@gmail.com>
2025-12-05 22:10:13 -0600

    Se le hizo modificaciones a este archivo

--- practicas/proyecto_tw/proyecto_tw/product_app/backend/product-list.php
+++ practicas/proyecto_tw/proyecto_tw/product_app/backend/product-list.php
@@ -1,8 +1,26 @@
 <?php
-    use TECWEB\MYAPI\Read\ProductList;
-    require_once __DIR__ . '/../vendor/autoload.php';
+header('Content-Type: application/json; charset=utf-8');
 
-    $productos = new ProductList('marketzone');
-    $productos->list();
-    echo $productos->getData();
-?>
+$cnx = @mysqli_connect('localhost', 'root', '', 'marketzone');
+
+if (!$cnx) {
+    echo json_encode([]);
+    exit;
+}
+
+$data = [];
+
+$sql = "SELECT id, titulo, descripcion, lenguaje, tipo_recurso, tipo_archivo, ruta_archivo
+        FROM archivos
+        ORDER BY fecha_alta DESC";
+
+if ($res = mysqli_query($cnx, $sql)) {
+    while ($row = mysqli_fetch_assoc($res)) {
+        $data[] = $row;
+    }
+    mysqli_free_result($res);
+}
+
+mysqli_close($cnx);
+
+echo json_encode($data);
